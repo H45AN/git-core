2f3b65cbebb6e9a301619d8e46a4d306a1e8e573
Junio C Hamano <gitster@pobox.com>
2015-08-07 09:41:54 -0700

    SQUASH???

--- sha1_file.c
+++ sha1_file.c
@@ -1446,7 +1446,6 @@ int git_open_noatime(const char *name)
 	static int sha1_file_open_flag = O_NOATIME;
 
 	for (;;) {
-		errno = 0;
 		int fd = open(name, O_RDONLY | sha1_file_open_flag);
 		if (fd >= 0)
 			return fd;
@@ -1454,6 +1453,7 @@ int git_open_noatime(const char *name)
 		/* Might the failure be due to O_NOATIME? */
 		if (errno != ENOENT && sha1_file_open_flag) {
 			sha1_file_open_flag = 0;
+			errno = 0;
 			continue;
 		}
 
